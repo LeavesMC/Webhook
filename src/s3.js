146df81dbaca13ab0dcb8601678c2a9487033969
LittleChest <hiya@littlew.top>
2025-06-20 23:07:11 +0800

    s3: Add download source callback

--- src/s3.js
+++ src/s3.js
@@ -12,11 +12,12 @@ const s3Client = new S3Client({
   signatureVersion: "v4",
 });
 
-async function upload(project, path, url) {
-  if (!project || !path || !url) {
+async function upload(project, tag, filename, url) {
+  if (!project || !tag || !filename || !url) {
     errlog("S3", "Missing parameters.");
     return { success: false, reason: "Missing parameters." };
   }
+  const path = `${tag}/${filename}`;
 
   const uploadParams = {
     Bucket: project,
@@ -24,10 +25,27 @@ async function upload(project, path, url) {
     Body: Buffer.from(await (await fetch(url)).arrayBuffer()),
   };
 
+  const commitParams = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "download_source": "rainyun",
+      project,
+      "url": path,
+      tag,
+    }),
+  };
+
   try {
     infolog("S3", `Starting upload to ${project}:/${path}`);
     await s3Client.send(new PutObjectCommand(uploadParams));
     infolog("S3", `Successfully uploaded ${path} to S3.`);
+    await fetch(
+      "https://api.leavesmc.org/v2/commit/build/download_source",
+      commitParams
+    );
     return;
   } catch (error) {
     errlog("S3", `Error uploading ${path} to S3: ${error.message}`);
